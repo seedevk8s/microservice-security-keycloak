e54a65d11b4cb4408bdf15c2e7f50d787a6e5ad6
seedevk8s <seedevk8s@gmail.com>
2024-05-05 05:23:28 +0000

    first commit

---
+++ restaurant-service/src/main/java/com/test/service/RestaurantService.java
@@ -0,0 +1,21 @@
+package com.test.service;
+
+import com.test.dao.RestaurantOrderDAO;
+import com.test.dto.OrderResponseDTO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestaurantService {
+    @Autowired
+    private RestaurantOrderDAO orderDAO;
+
+    public String greeting() {
+        return "Welcome to Swiggy Restaurant service";
+    }
+
+    public OrderResponseDTO getOrder(String orderId) {
+        return orderDAO.getOrders(orderId);
+    }
+}
---
+++ swiggy-app/src/main/java/com/test/service/SwiggyAppService.java
@@ -0,0 +1,22 @@
+package com.test.service;
+
+import com.test.client.RestaurantServiceClient;
+import com.test.dto.OrderResponseDTO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SwiggyAppService {
+
+    @Autowired
+    private RestaurantServiceClient restaurantServiceClient;
+
+    public String greeting() {
+        return "Welcome to Swiggy App Service";
+    }
+
+    public OrderResponseDTO checkOrderStatus(String orderId) {
+        return restaurantServiceClient.fetchOrderStatus(orderId);
+    }
+}
